697cea9e4fa68abf5e0f6e9e3548baf0b1108ea6
DasBrin <45804323+DasBrin@users.noreply.github.com>
2024-10-14 18:00:04 +0200

    Update quota-files-sidebar.js
    Signed-off-by: DasBrin <45804323+DasBrin@users.noreply.github.com>

--- src/quota-files-sidebar.js
+++ src/quota-files-sidebar.js
@@ -1,3 +1,11 @@
+/**
+ * @copyright Copyright (c) 2023 Kate <jld3103yt@gmail.com>
+ * @license GNU AGPL version 3 or any later version
+ *
+ * SPDX-FileCopyrightText: 2023 Kate <jld3103yt@gmail.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import {loadState} from '@nextcloud/initial-state'
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -6,18 +14,26 @@ document.addEventListener('DOMContentLoaded', () => {
 	for (const site of state) {
 		const image = document.createElement('img')
 		image.src = site.image
+		image.style.add('max-width:20px;max-height:20px')
+
+		const imagespan = document.createElement('span')
+		imagespan.classList.add('material-design-icon')
+		imagespan.append(image)
 
 		const icon = document.createElement('div')
 		icon.classList.add('app-navigation-entry-icon')
-		icon.append(image)
+		icon.data-v-c00d5366.add('')
+		icon.append(imagespan)
 
 		const name = document.createElement('span')
 		name.classList.add('app-navigation-entry__name')
 		name.innerText = site.name
 
 		const a = document.createElement('a')
-		a.classList.add('app-navigation-entry-link')
 		a.href = site.href
+		a.classList.add('app-navigation-entry-link')
+		a.title = site.name
+		a.data-v-c00d5366.add('')
 		a.append(icon)
 		a.append(name)
 
